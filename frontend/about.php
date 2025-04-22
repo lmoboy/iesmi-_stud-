2adc0ca9b07f04cd831ddd27e9f3e051dbf2a085
lmoboy <steamgoyu19@gmail.com>
2025-04-22 13:20:08 +0300

    refactor: update about page layout and styling; remove unnecessary elements

--- frontend/about.php
+++ frontend/about.php
@@ -1,16 +1,15 @@
 <?php
 ?>
-
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Big dick nigger</title>
-</head>
 <body>
-    nigga balls
+    <div class="card m-4 w-80 shadow-sm">
+        <figure>
+            <img src="https://img.daisyui.com/images/blog/daisyui-5.webp" />
+        </figure>
+        <div class="card-body">
+            <h2 class="card-title">DaisyUI 5.0</h2>
+            <p>Rerum reiciendis beatae tenetur excepturi aut pariatur est eos. Sit sit necessitatibus.</p>
+        </div>
+    </div>
 </body>
+
 </html>
--- frontend/layouts/admin.php
+++ frontend/layouts/admin.php
@@ -5,11 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>IESMINS_STUDE</title>
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
+    <script defer src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
 
-<body style="background: black; color: white;">
+<body class="bg-base-200">
     <?= $content ?>
-
 </body>
 
 </html>
