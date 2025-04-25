7271e6dd5c2a02e121a9b19a8c15136d209dfc7f
lmoboy <steamgoyu19@gmail.com>
2025-04-25 10:53:25 +0300

    LOGGING UPDATE S

--- utils/logging.php
+++ utils/logging.php
@@ -6,19 +6,15 @@ function debug_log($message, $type = 'info')
 
     $file = debug_backtrace();
     $os = PHP_OS_FAMILY;
+    $file = $file[0]['file'];
     if ($os === "Darwin") {
-        $file = $file[0]['file'];
         $file = explode('/', $file);
-        $file = $file[count($file) - 1];
-        $file = explode('.php', $file)[0];
-        $file = strtoupper($file);
     } else if ($os === "Windows") {
-        $file = $file[0]['file'];
         $file = explode('\\', $file);
-        $file = $file[count($file) - 1];
-        $file = explode('.php', $file)[0];
-        $file = strtoupper($file);
     }
+    $file = $file[count($file) - 1];
+    $file = explode('.php', $file)[0];
+    $file = strtoupper($file);
 
     $log_message = date('[Y-m-d H:i:s]') . " [{$file}] [{$type}] {$message}\n";
     error_log($log_message, 3, DEBUG_LOG_FILE);
